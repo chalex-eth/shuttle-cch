d19f6e8b572ba49ef513c229697d3bc008f9ac54
chalex-eth <chalard.alex@gmail.com>
2023-12-04 13:18:02 +0100

    Challenge completed

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 use axum::{
-    http::StatusCode,
+    extract::Path,
+    //http::StatusCode,
     response::{IntoResponse, Response},
     routing::get,
     Router,
 };
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
-
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new()
-        .route("/", get(hello_world))
-        .route("/-1/error", get(error));
+    let router = Router::new().route("/1/*path", get(result));
 
     Ok(router.into())
 }
 
-async fn error() -> Response {
-    StatusCode::INTERNAL_SERVER_ERROR.into_response()
+async fn result(Path(input): Path<String>) -> Response {
+    let nums: Vec<i32> = input
+        .split('/')
+        .filter_map(|s| s.parse::<i32>().ok())
+        .collect();
+    let result = nums.iter().fold(0, |acc, &num| acc ^ num).pow(3);
+    println!("result: {}", result);
+    result.to_string().into_response()
 }
